ae1552c74e8bee73e795f1f0de797e7711e78ffe
ralph kemp <ralphk3mp@gmail.com>
2018-08-31 14:48:31 +0100

    improved video icon functionality, still more to do though.

--- videoControls.js
+++ videoControls.js
@@ -4,24 +4,40 @@ const muteButton = document.getElementById('mute');
 const fullScreenButton = document.getElementById('full-screen');
 const seekBar = document.getElementById('seek-bar');
 const volumeBar = document.getElementById('volume-bar');
+const vidClickContainer = document.querySelector('.vid-click-container');
+
+// first add logic to show and hide play image and pause image. Then
+// add hover logic. change the images for mute too.
+
+TweenMax.to('#video, #play-pause', 1.5, { opacity: 0.9 });
 
 // controls
-playButton.addEventListener('click', function() {
-  if (video.paused == true) {
-    video.play();
-    playButton.innerHTML = 'Pause';
-  } else {
-    video.pause();
-    playButton.innerHTML = 'Play';
-  }
-});
+  vidClickContainer.addEventListener('click', function() {
+    // playButton.style.display = "none";
+    if (video.paused == true) {
+      video.play();
+      playButton.style.display = 'none';
 
-muteButton.addEventListener("click", function() {
+      // show on hover
+      vidClickContainer.addEventListener('mouseover', () => {
+        playButton.style.display = 'block';
+      });
+      vidClickContainer.addEventListener('mouseout', () => {
+        playButton.style.display = 'none';
+      });
+    } else {
+      video.pause();
+      playButton.style.display = 'block';
+    }
+  });
+
+
+muteButton.addEventListener('click', function() {
   if (video.muted == false) {
     video.muted = true;
-    muteButton.innerHTML = "Unmute";
+    muteButton.innerHTML = 'Unmute';
   } else {
     video.muted = false;
-    muteButton.innerHTML = "Mute";
+    muteButton.innerHTML = 'Mute';
   }
 });
