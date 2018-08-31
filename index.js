a601d8711a1b14fabaf87cca7dc2ca2fc3920c2d
ralph kemp <ralphk3mp@gmail.com>
2018-08-31 16:46:34 +0100

    trying to override slides when i click on a rec, function isn't being overwritten though, hmm

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ const bodyFour = document.getElementById('body-four');
 const bodies = [bodyOne, bodyTwo, bodyThree, bodyFour];
 const rectangles = [recOne, recTwo, recThree, recFour];
 
+const runSlides = () => {
+  const tl = new TimelineLite();
+  tl.to(bodyOne, 0.3, { opacity: 0, delay: 4 })
+    .call(toggleColor, [recTwo], this, 3.4)
+    .call(toggleColor, [recOne], this, 3.4)
+    .to(bodyTwo, 0.3, { opacity: 1 }, '-=1')
+    .to(bodyTwo, 0.3, { opacity: 0, delay: 4 })
+    .call(toggleColor, [recThree], this, 7.6)
+    .call(toggleColor, [recTwo], this, 7.6)
+    .to(bodyThree, 0.3, { opacity: 1 }, '-=1')
+    .to(bodyThree, 0.3, { opacity: 0, delay: 4 })
+    .call(toggleColor, [recFour], this, 11)
+    .call(toggleColor, [recThree], this, 11)
+    .to(bodyFour, 0.3, { opacity: 1 }, '-=1');
+};
+
+runSlides();
+
 function toggleColor(rec) {
   return rec.classList.toggle('selected');
 }
@@ -28,33 +46,17 @@ function trigger(arr, elem, styleChange, styleChangeTwo) {
 // EventListeners for each rectangle. When rec is clicked,
 // we call the trigger which use rec and body, then set the
 // styles of the objects we're on, as opposed to the newArray objs.
-function listener(rec, body) {
+function listener(rec, body, func) {
   rec.addEventListener('click', () => {
     trigger(bodies, body, 0, null);
     trigger(rectangles, rec, null, '#705c1a');
     body.style.opacity = 1;
     rec.style.backgroundColor = '#ffd400';
+    func = function() {};
   });
 }
 
-listener(recOne, bodyOne);
-listener(recTwo, bodyTwo);
-listener(recThree, bodyThree);
-listener(recFour, bodyFour);
-
-// loop
-
-const tl = new TimelineLite();
-
-tl.to(bodyOne, 0.3, { opacity: 0, delay: 4 })
-  .call(toggleColor, [recTwo], this, 3.4)
-  .call(toggleColor, [recOne], this, 3.4)
-  .to(bodyTwo, 0.3, { opacity: 1 }, '-=1')
-  .to(bodyTwo, 0.3, { opacity: 0, delay: 4 })
-  .call(toggleColor, [recThree], this, 7.6)
-  .call(toggleColor, [recTwo], this, 7.6)
-  .to(bodyThree, 0.3, { opacity: 1 }, '-=1')
-  .to(bodyThree, 0.3, { opacity: 0, delay: 4 })
-  .call(toggleColor, [recFour], this, 11)
-  .call(toggleColor, [recThree], this, 11)
-  .to(bodyFour, 0.3, { opacity: 1 }, '-=1');
+listener(recOne, bodyOne, runSlides);
+listener(recTwo, bodyTwo, runSlides);
+listener(recThree, bodyThree, runSlides);
+listener(recFour, bodyFour, runSlides);
